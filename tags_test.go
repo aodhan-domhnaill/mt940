018a42d127557727f82f5af679d0590140764eb3
Aidan Macdonald <aidan@medic.org>
2023-04-25 19:23:34 +0300

    test for tags

--- tags_test.go
+++ tags_test.go
@@ -9,11 +9,18 @@ func TestTag_Parse(t *testing.T) {
 		t.Run(tag.name, func(t *testing.T) {
 			for _, ex := range tag.examples {
 				out, err := tag.Parse(ex)
+				groupNames := tag.re.SubexpNames()
 				if err != nil {
 					t.Error(err)
 				}
 				if out == nil {
 					t.Error("output was nil")
+				}
+				for _, g := range groupNames {
+					_, ok := out[g]
+					if !ok {
+						t.Errorf("group %v not found", g)
+					}
 				}
 			}
 		})
